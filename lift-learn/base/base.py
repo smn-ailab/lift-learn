852fcf8737363d71eb15e1b37a7af618a3842334
Yuta Saito <elpistolero317@gmail.com>
2019-02-12 09:52:08 +0900

    added an abstract base class for all uplift models

---
+++ lift-learn/base/base.py
@@ -0,0 +1,63 @@
+"""Base classes for all uplift models."""
+from abc import ABC, abstractmethod
+
+import numpy as np
+from sklearn.base import BaseEstimator
+
+
+class UpliftModelInterface:
+    """Abstract base class for all uplift models in lift-learn."""
+
+    @abstractmethod
+    def fit(self, X: np.ndarray, y: np.ndarray, w: np.ndarray) -> None:
+        """Build an uplift model from the training set (X, y, w).
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape = [n_samples, n_features]
+            The training input samples. Sparse matrices are accepted only if
+            they are supported by the base estimator.
+        y : array-like, shape = [n_samples]
+            The target values (class labels in classification, real numbers in
+            regression).
+        w : array-like, shape = [n_samples]
+            The treatment assignment.
+
+        """
+        pass
+
+    @abstractmethod
+    def predict(self, X: np.ndarray) -> None:
+        """Predict optimal treatment for X.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape = [n_samples, n_features]
+            The test input samples. Sparse matrices are accepted only if
+            they are supported by the base estimator.
+
+        Returns
+        -------
+        t : array of shape = [n_samples]
+            The predicted optimal treatments.
+
+        """
+        pass
+
+    @abstractmethod
+    def predict_ite(self, X: np.ndarray) -> None:
+        """Predict individual treatment effects for X.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape = [n_samples, n_features]
+            The test input samples. Sparse matrices are accepted only if
+            they are supported by the base estimator.
+
+        Returns
+        -------
+        ite : array of shape = [n_samples, (n_trts - 1)]
+            The predicted individual treatment effects.
+
+        """
+        pass
