51085b5cfc1971abd159fa75434d024ab8efbf22
Yuta Saito <elpistolero317@gmail.com>
2019-02-15 11:46:46 +0900

    implemented PropensityBasedModel class

--- lflearn/base.py
+++ lflearn/base.py
@@ -1,8 +1,9 @@
 """Base classes for all uplift models."""
 from abc import ABC, abstractmethod
+from typing import Optional, Union
 
 import numpy as np
-from sklearn.base import BaseEstimator
+from sklearn.base import BaseEstimator, RegressorMixin, ClassifierMixin
 
 
 class UpliftModelInterface:
@@ -63,3 +64,66 @@ class UpliftModelInterface:
 
         """
         pass
+
+
+class PropensityBasedModel(BaseEstimator, UpliftModelInterface):
+    """Base class for all propensity based uplift models in lift-learn."""
+
+    def __init__(self,
+                 base_model: Union[ClassifierMixin, RegressorMixin],
+                 ps_model: Optional[ClassifierMixin]=None) -> None:
+        """Initialize Class."""
+        self.base_model = base_model
+        self.ps_model = ps_model
+
+    def predict_ite(self, X: np.ndarray) -> np.ndarray:
+        """Predict individual treatment effects for X.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix} of shape = [n_samples, n_features]
+            The test input samples. Sparse matrices are accepted only if
+            they are supported by the base estimator.
+
+        Returns
+        -------
+        ite : array of shape = [n_samples, (n_trts - 1)]
+            The predicted individual treatment effects.
+
+        """
+        return self.base_model.predict(X)
+
+    def _transform_outcome(y: np.ndarray, w: np.ndarray, ps: np.ndarray,
+                           mu: Optional[np.ndarray]=None, gamma: float=0.0) -> np.ndarray:
+        """Calcurate Transformed Outcomes.
+
+        Parameters
+        ----------
+        y : array-like, shape = [n_samples]
+            The target values (class labels in classification, real numbers in
+            regression).
+
+        w : array-like, shape = [n_samples]
+            The treatment assignment.
+
+        ps: array-like, shape = [n_samples]
+            The estimated propensity scores.
+
+        mu: array-like, shape = [n_samples]
+            The estimated potential outcomes.
+
+        gamma: float, optional (default=0.0)
+
+        Returns
+        ----------
+        transformed_outcome: array-like, shape = [n_samples]
+            The transformed outcomes.
+
+        """
+        mu = np.zeros((y.shape[0], 2)) if mu is None else mu
+
+        direct_estimates = mu[:, 1] - mu[:, 0]
+        transformed_outcome = w * (y - mu[:, 1]) / ps[:, 1] - (1 - w) * (y - mu[:, 0]) / ps[:, 0] + direct_estimates
+        transformed_outcome[(w == 1) & (ps[:, 1] < gamma)] = direct_estimates[(w == 1) & (ps[:, 1] < gamma)]
+        transformed_outcome[(w == 0) & (ps[:, 0] < gamma)] = direct_estimates[(w == 0) & (ps[:, 0] < gamma)]
+        return transformed_outcome
